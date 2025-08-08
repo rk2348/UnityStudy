fadbd981f3014f450034111d6f3789943499037e
rk2348 <ryoma.k0718@icloud.com>
2025-08-09 00:08:52 +0900

    変数をイベント変数に変更

--- Assets/Scripts/Array.cs
+++ Assets/Scripts/Array.cs
@@ -5,40 +5,48 @@ using UnityEngine;
 
 public class Array : MonoBehaviour
 {
-    private int[] Num = new int[3];
-    //private int[] Num1 = new int[]{1,2,3};
-    //private int[] Num2 = { 1, 2, 3 };
-
     void Start()
     {
-        Num = new int[] { 1, 2, 3 };
+        ClearArray();
     }
 
-    void Update()
-    {
-
-    }
 
     void Log() //配列の中身を出力したいとき
     {
+        int[] Num = new int[3];
+        Num = new int[3]{ 1, 2, 3 };
+
         Debug.Log(Num[2]);
     }
 
+
     void ClearArray() //要素数0の空の配列を代入したい時
     {
+        int[] Num = new int[] { 1, 2, 3 };
+
         Num = new int[0];
     }
 
+
     void ResetArrayValue() //配列の中身を初期化したいとき
     {
+        int[] Num = { 1, 2, 3 };
+
         for(int i = 0; i < Num.Length; i++)
         {
             Num[i] = 0;
         }
+
+        Debug.Log(Num[0]);
+        Debug.Log(Num[1]);
+        Debug.Log(Num[2]);
     }
+
 
     void DeleteArray() //配列を完全に消去したいとき
     {
+        int[] Num = { 1, 2, 3 };
+
         Num = null;
     }
 }
